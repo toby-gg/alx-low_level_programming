ef4840eb16b57c85d6884a8a76555ac7eff5ddd6
toby-gg <oshiboteoluwatobi@gmail.com>
2023-03-22 18:02:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,15 +13,7 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('_putchar');
 	_putchar('\n');
 
 	return (0);
